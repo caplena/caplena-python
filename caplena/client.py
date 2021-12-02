aab0f4ff9e1492dc68247d4ae159b4645835f9c4
Nico Haenggi <nico@haenggi.ch>
2021-12-02 15:33:34 +0100

    (refactor) refactored root api client

--- caplena/client.py
+++ caplena/client.py
@@ -1,24 +1,41 @@
-from .configuration import Configuration, Environment
-from .controllers.projects_controller import ProjectsController
+from typing import Type
+
+from caplena.api import ApiBaseUri, ApiVersion
+from caplena.configuration import Configuration
+from caplena.controllers.projects_controller import ProjectsController
+from caplena.http.http_client import HttpClient
+from caplena.http.requests_http_client import RequestsHttpClient
+from caplena.logging import LoggingLevel
 
 
 class Client:
+    @property
     def projects(self):
-        return ProjectsController(self.config)
+        return ProjectsController(config=self.config)
+
+    @property
+    def config(self):
+        return self._config
 
     def __init__(
         self,
         api_key: str,
         *,
+        api_base_uri: ApiBaseUri = ApiBaseUri.PRODUCTION,
+        api_version: ApiVersion = ApiVersion.DEFAULT,
         timeout: int = 30,
-        max_retries: int = 1,
+        max_retries: int = 0,
         backoff_factor: int = 2,
-        environment: Environment = Environment.PRODUCTION,
+        http_client_class: Type[HttpClient] = RequestsHttpClient,
+        logging_level: LoggingLevel = LoggingLevel.WARNING,
     ):
-        self.config = Configuration(
+        self._config = Configuration(
             api_key=api_key,
+            api_base_uri=api_base_uri,
+            api_version=api_version,
             timeout=timeout,
             max_retries=max_retries,
             backoff_factor=backoff_factor,
-            environment=environment,
+            http_client_class=http_client_class,
+            logging_level=logging_level,
         )
