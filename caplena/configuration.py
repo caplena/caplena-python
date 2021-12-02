052bf0142523e77528735405d97ee4b1fce59859
Nico Haenggi <nico@haenggi.ch>
2021-12-02 15:31:52 +0100

    (refactor) dependency inversion for http clients in configuration

--- caplena/configuration.py
+++ caplena/configuration.py
@@ -1,15 +1,22 @@
-from enum import Enum
+from typing import Type
 
-
-class Environment(Enum):
-    PRODUCTION = 0
-    LOCAL = 1
+from caplena.api import ApiBaseUri, ApiRequestor, ApiVersion
+from caplena.http.http_client import HttpClient
+from caplena.logging import LoggingLevel
 
 
 class Configuration:
     @property
     def api_key(self):
         return self._api_key
+
+    @property
+    def api_base_uri(self):
+        return self._api_base_uri
+
+    @property
+    def api_version(self):
+        return self._api_version
 
     @property
     def timeout(self):
@@ -24,20 +31,33 @@ class Configuration:
         return self._backoff_factor
 
     @property
-    def environment(self):
-        return self._environment
+    def logging_level(self):
+        return self._logging_level
+
+    @property
+    def api_requestor(self):
+        return self._api_requestor
 
     def __init__(
         self,
         *,
         api_key: str,
-        timeout: int = 60,
-        max_retries: int = 1,
+        api_base_uri: ApiBaseUri = ApiBaseUri.PRODUCTION,
+        api_version: ApiVersion = ApiVersion.DEFAULT,
+        timeout: int = 30,
+        max_retries: int = 0,
         backoff_factor: int = 2,
-        environment: Environment = Environment.PRODUCTION
+        http_client_class: Type[HttpClient],
+        logging_level: LoggingLevel = LoggingLevel.WARNING,
     ):
         self._api_key = api_key
+        self._api_base_uri = api_base_uri
+        self._api_version = api_version
+        self._http_client_class = http_client_class
         self._timeout = timeout
         self._max_retries = max_retries
         self._backoff_factor = backoff_factor
-        self._environment = environment
+        self._logging_level = logging_level
+
+        self._http_client = http_client_class()
+        self._api_requestor = ApiRequestor(http_client=self._http_client)
