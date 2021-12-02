a87004283bc28609e2006de30a87732bae6d6cd1
Nico Haenggi <nico@haenggi.ch>
2021-12-02 15:36:00 +0100

    (tests) addd user agent test

---
+++ tests/test_helpers.py
@@ -0,0 +1,9 @@
+import unittest
+
+from caplena.helpers import Helpers
+
+
+class HelperTests(unittest.TestCase):
+    def test_user_agent(self):
+        user_agent = Helpers.get_user_agent(identifier="requests")
+        self.assertRegex(user_agent, r"requests/0\.0\.\d python/3\.\d\.\d")
