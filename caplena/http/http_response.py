9f54a67fbe507b0f69464ca55e97d616c14af7ed
Nico Haenggi <nico@haenggi.ch>
2021-12-03 18:30:48 +0100

    (feature) implemented proper http response

--- caplena/http/http_response.py
+++ caplena/http/http_response.py
@@ -1,7 +1,17 @@
+import json
 from typing import Any, Dict, Optional
 
 
 class HttpResponse:
+    @property
+    def json(self) -> Optional[Dict[str, Any]]:
+        if self._json is not None:
+            return self._json
+        elif self.text:
+            self._json = json.loads(self.text)
+        else:
+            return None
+
     def __init__(
         self,
         *,
@@ -10,8 +20,13 @@ class HttpResponse:
         text: Optional[str] = None,
         headers: Optional[Dict[str, str]] = None,
     ):
-        pass
+        self.status_code = status_code
+        self.reason = reason
+        self.text = text
+        self.headers = headers
 
-    def json(self) -> Dict[str, Any]:
-        # TODO: implement json parsing
-        return {}
+    def __str__(self):
+        concat_text = (
+            self.text[:50] + "..." if self.text and len(self.text) > 50 else str(self.text)
+        )
+        return f"HttpResponse(status_code={self.status_code}, reason='{self.reason}', text='{concat_text}')"
