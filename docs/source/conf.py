11ce6b52ef3da7040ed8a6fb85f6027cb8552761
Aliaksei Kuchyk <aliaksei@caplena.com>
2024-04-02 16:43:23 +0200

    fix(APP-3160): update version

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -22,8 +22,8 @@ sys.path.insert(0, os.path.abspath("../.."))
 project = "Caplena"
 copyright = "2024, Caplena"
 author = "Caplena"
-version = "2.0.0"
-release = "2.0.0"
+version = "2.0.1"
+release = "2.0.1"
 
 
 # -- General configuration ---------------------------------------------------
