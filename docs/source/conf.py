63b55912c4a23007e543014f9e2a62dceacc6164
Aliaksei Kuchyk <aliaksei@caplena.com>
2024-10-30 16:22:59 +0100

    fix(app-3632): fix project urls

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -22,8 +22,8 @@ sys.path.insert(0, os.path.abspath("../.."))
 project = "Caplena"
 copyright = "2024, Caplena"
 author = "Caplena"
-version = "2.1.0"
-release = "2.1.0"
+version = "2.1.1"
+release = "2.1.1"
 
 
 # -- General configuration ---------------------------------------------------
